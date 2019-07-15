603ae4252b21e73d28edef73ab3a1dbd91948b18
Karl <karlmagnus.lundin@gmail.com>
2019-07-15 21:38:33 +0200

    removed all unused logs from project

--- Logs/LogsCLTool/From CLTool/FirstTest0211/README.txt
+++
@@ -1,5 +0,0 @@
-These logs were collected using -msgINS1, -msgPIMU
-Two issues were observed:
-First: Only time in seconds was collected.
-Second: Positions were given in straight lines in horizontal solution. The behaviour should be near-continuously 
-stochastic, not discrete.
--- Logs/LogsCLTool/relativePosIMU.m
+++
@@ -1,9 +0,0 @@
-%Position from IMU-data, attempt at estimating position from data collected
-%by IMU-sensors
-clear all, close all
-%T1=readtable('20190214_160038\LOG_SN34090_20190214_160038_0001_gps1Pos.csv');
-%T2=readtable('20190214_160038\LOG_SN34072_20190214_160038_0001_gps1Pos.csv');
-T1=readtable('20190214_160038\LOG_SN34072_20190214_160038_0001_preintegratedImu.csv');
-T2=readtable('20190214_160038\LOG_SN34072_20190214_160038_0001_preintegratedImu.csv');
-%%
-posFromIMU(T1,T2, 1)
